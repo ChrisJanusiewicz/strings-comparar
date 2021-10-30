10e0885febfb6ca74a5fb0f3145b1dd6622db203
chris <kpjanusiewicz@gmail.com>
2021-10-30 22:55:52 +0100

    feat: support ignoring case when comparing strings

--- comparestrings.py
+++ comparestrings.py
@@ -1,6 +1,9 @@
 import numpy as np
+import re
 from enum import Enum
 import argparse
+
+separators_regex = r'[\s\.\-]+'
 
 
 class Operation(Enum):
@@ -9,10 +12,17 @@ class Operation(Enum):
     DELETION = 3
 
 
-def dist(s, t, split_words=True, deletion_cost=1, insertion_cost=1, substitution_cost=1):
+def compare(s, t, case_sensitive=True):
+    if case_sensitive:
+        return s == t
+    else:
+        return s.lower() == t.lower()
+
+
+def dist(s, t, split_words=True, case_sensitive=True, deletion_cost=1, insertion_cost=1, substitution_cost=1):
     if split_words:
-        s = s.split(' ')
-        t = t.split(' ')
+        s = re.split(separators_regex, s)
+        t = re.split(separators_regex, t)
 
     m = len(t) + 1
     n = len(s) + 1
@@ -27,14 +37,14 @@ def dist(s, t, split_words=True, deletion_cost=1, insertion_cost=1, substitution
 
     for j in range(1, n):
         for i in range(1, m):
-            if s[j-1] == t[i-1]:
+            if compare(s[j-1], t[i-1], case_sensitive):
                 sub = 0
             else:
                 sub = substitution_cost
 
             d[i, j] = min(
-                d[i-1, j] + 1,
-                d[i, j-1] + 1,
+                d[i-1, j] + insertion_cost,
+                d[i, j-1] + deletion_cost,
                 d[i-1, j-1] + sub
             )
 
@@ -79,12 +89,12 @@ def dist(s, t, split_words=True, deletion_cost=1, insertion_cost=1, substitution
     while stack:
         op, value = stack.pop()
         if op == Operation.EQUAL:
-                md_s += f'{value.lower()}' + separator
-                md_t += f'{value.lower()}' + separator
+                md_s += f'{value}' + separator
+                md_t += f'{value}' + separator
         elif op == Operation.INSERTION:
-                md_t += f'{value.upper()}' + separator
+                md_t += f'{value}' + separator
         elif op == Operation.DELETION:
-                md_s += f'{value.upper()}' + separator
+                md_s += f'{value}' + separator
 
     with open('out.md', 'w+') as outfile:
         outfile.write(f'{md_s}\n\n')
@@ -93,9 +103,9 @@ def dist(s, t, split_words=True, deletion_cost=1, insertion_cost=1, substitution
     return d[m-1, n-1]
 
 
-def main(s, t, split_words):
+def main(s, t, split_words, case_sensitive):
     print(f'{s} {t}')
-    result = dist(s, t, split_words, 1, 1, 1)
+    result = dist(s, t, split_words, case_sensitive, 1, 1, 1)
     print(f'Minimum edit distance: {result}')
 
 
@@ -105,7 +115,8 @@ if __name__ == '__main__':
     parser.add_argument('string2', help='The second string to compare')
     parser.add_argument("-w", "--word", help="Perform comparison on whole words, " +
         "as opposed to on individual characters.", action='store_true')
+    parser.add_argument("-i", "--ignore-case", help="Whether ignore case " +
+        "when performing comparisons.", action='store_true')
     args = parser.parse_args()
-    print(args.word)
 
-    main(args.string1, args.string2, args.word)
+    main(args.string1, args.string2, args.word, not args.ignore_case)
